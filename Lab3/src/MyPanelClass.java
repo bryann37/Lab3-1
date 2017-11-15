7cac9b82814a82962463b737fd54e3a197f95949
4010d4 <4010d4@icaco.ece.uprm.edu>
2017-11-15 15:03:37 -0400

    Create a Circle that is fill with color and is print in the center

---
+++ Lab3/src/MyPanelClass.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.JPanel;
+ 
+public class MyPanelClass extends JPanel {
+            /**
+	 * 
+	 */
+	private static final long serialVersionUID = 7057541440811488699L;
+
+			public void paintComponent(Graphics g) {
+                        super.paintComponent(g);
+ 
+                        //Compute interior coordinates
+                        Insets myInsets = this.getInsets();
+                        int x1 = myInsets.left;
+                        int y1 = myInsets.top;
+                        int x2 = getWidth() - myInsets.right - 1;
+                        int y2 = getHeight() - myInsets.bottom - 1;
+                        int width = x2 - x1;
+                        int height = y2 - y1;
+ 
+                        //Paint the background
+                        g.setColor(Color.darkGray);
+                        g.fillRect(x1, y1, width + 1, height + 1);
+                        
+                 
+//                        //Draw a border1
+//                        g.setColor(Color.YELLOW);
+//                        g.drawRect(x1, y1, width, height);
+//                        
+//                        //Draw a border2 inside the border 1
+//                        g.setColor(Color.red);
+//                        g.drawRect(x1 + 6, y2 -95, width -10, height -10);
+//                        
+//                        g.setColor(Color.WHITE);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        
+//                        g.setColor(Color.pink);
+//                        g.drawLine(x2, y1, x1, y2);
+                        
+                        g.setColor(Color.LIGHT_GRAY);
+                        g.drawOval(x1 + 170, y1 + 20, 55, 55);
+                        g.fillOval(x1 + 170, y1 + 20, 55, 55);
+                        
+                        
+            }
+}
