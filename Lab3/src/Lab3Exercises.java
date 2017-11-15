83395922342f27f84069a0821528bc6d034ed521
4010d4 <4010d4@icaco.ece.uprm.edu>
2017-11-15 14:14:39 -0400

    This code have the change of the location when you run the class and have my name.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,9 +2,9 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
+		JFrame myFrame = new JFrame("Bryan N. Bonilla Rodriguez");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(800, 475);
 		myFrame.setSize(400, 100);
 		myFrame.setVisible(true);
 	}
